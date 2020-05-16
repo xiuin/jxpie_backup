ebe59b24961ae20f552efcc34f35dda233d9f10a
xiuxiuxiu <xiu@o2fun.com>
2020-05-17 00:49:59 +0800

    fix(js): add target

--- js/custom.js
+++ js/custom.js
@@ -27,19 +27,19 @@ function createTable(dataType, data) {
                     '<td><span>' + parseInt(x.value)/1000 + 'w 影响力</span></td></tr>'
         }  else if (dataType == 4) {
             s = s + '<tr><td>' + x.rank + '.</td>' +
-                    '<td><a href="'+ x.link +'">' +
+                    '<td><a target="_blank" href="'+ x.link +'">' +
                     '<img style="height: 20%" src="'+ x.img_url + '"></a></td>' +
                     '<td><span style="white-space: normal;">' + x.title + '</span></td>' +
                     '<td><span><i class="icon ion-heart"></i>' + parseInt(x.value)/1000 + 'w</span></td></tr>'
         } else if (dataType == 5 || dataType == 6 || dataType == 7) {
             s = s + '<tr><td>' + x.rank + '.</td>' +
-                    '<td><img style="height: 80%" src="'+ x.img_url + '"></a></td>' +
+                    '<td><img style="height: 80%" src="'+ x.img_url + '"></td>' +
                     '<td><span style="white-space: normal;">' + x.title + ' [' + secondToDate(x.duration) + ']</span></td>' +
                     '<td>' + x.author + '</td>' +
                     '<td><span>' + parseInt(x.value)/1000 + 'w 人使用</span></td></tr>'
         } else if (dataType == 10) {
             s = s + '<tr><td>' + x.rank + '.</td>' +
-                    '<td><img style="height: 5%" src="'+ x.img_url + '"></a></td>' +
+                    '<td><img style="height: 5%" src="'+ x.img_url + '"></td>' +
                     '<td><span style="white-space: normal;">' + x.title + ' [' + x.author + ']</span></td>' +
                     '<td><span><i class="icon ion-fireball"></i>' + parseInt(x.value)/1000 + 'w</span></td></tr>'
         }    else {}
